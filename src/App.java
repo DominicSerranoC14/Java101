c666af3e87c749e1154582b847235483b6dd6ee8
Dominic Serrano <dmsrojo@gmail.com>
2017-01-24 10:25:33 -0600

    Initial

---
+++ src/App.java
@@ -0,0 +1,25 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int myNum = 88;
+		double myDouble = 33.33;
+		float myFloat = 33.3f;
+		
+		char myChar = 'y';
+		
+		boolean myBool = true;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNum);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myByte);
+		
+	}
+
+}
